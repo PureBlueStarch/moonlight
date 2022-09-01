0a1371e7fda64f077dfeaf01ce3339ffe9f55b9c
potato <k2387043371@163.com>
2022-09-01 16:52:56 +0800

    变量

---
+++ test3/test3/test3.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int age = 20;
+//	//Int---数据类型
+//	//age--- 变量的名字
+//	//= ---赋值
+//	//== --- 等于
+//	float weight = 50.1;
+//	age = age + 1;
+//	weight = weight - 2;
+//	printf("%d\n", age);
+//	printf("%f\n", weight);
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n", sum);
+	return 0;
+}
